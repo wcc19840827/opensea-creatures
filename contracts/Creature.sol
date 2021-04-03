086ab949c833df3a72d41a6ded123b1d0a75d7a3
ryan-xeniro <ryan.wu@xeniro.com>
2021-04-04 01:01:55 +0800

    use github

--- contracts/Creature.sol
+++ contracts/Creature.sol
@@ -13,10 +13,16 @@ contract Creature is ERC721Tradable {
         ERC721Tradable("Creature", "OSC", _proxyRegistryAddress)
     {}
 
-    function baseTokenURI() public pure returns (string memory) {
-        //如果您使用IPFS托管元数据，则URL的格式应为ipfs://<hash>。例如，ipfs://QmTy8w65yBXgyfG2ZBg5TrfB2hPjrDQH3RCQFJGkARStJb
-        return "https://ipfs.io/ipfs/QmXeypeQzWsbiBbeo7vKuGvaRvrp9j6NGcsPWegZmW8XXy/metadata/"; //TODO:
+    function baseTokenURI() public pure returns (string memory) { //TODO:修改成自己的 元数据服务器的API
+        // 官方
         // return "https://creatures-api.opensea.io/api/creature/";
+
+        // 如果您使用IPFS托管元数据，则URL的格式应为ipfs://<hash>。例如，ipfs://QmTy8w65yBXgyfG2ZBg5TrfB2hPjrDQH3RCQFJGkARStJb
+        // 缺点是不易扩展，每次修改元数据后都会产生新的CID
+        // return "https://ipfs.io/ipfs/QmXeypeQzWsbiBbeo7vKuGvaRvrp9j6NGcsPWegZmW8XXy/metadata/";
+
+        // 使用 github
+        return "https://raw.githubusercontent.com/wcc19840827/opensea-creatures/ryan/dev/res/metadata/";
     }
 
     function contractURI() public pure returns (string memory) {
